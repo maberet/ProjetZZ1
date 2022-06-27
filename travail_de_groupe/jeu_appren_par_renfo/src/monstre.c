ca38c3b5b21ddcf4ecf9dba84655d8570cd7924c
maberet <maberet@turing.local.isima.fr>
2022-06-27 12:01:21 +0200

    Modification des noms de variables bis

--- travail_de_groupe/jeu_appren_par_renfo/src/monstre.c
+++ travail_de_groupe/jeu_appren_par_renfo/src/monstre.c
@@ -8,8 +8,8 @@ void  initMonster (){
 
     srand(time(NULL)); 
 
-    monster.x= (rand()%((MAPSIZEX-3)/2)+16)*CELLSIZE;
-    monster.y= (rand()%(MAPSIZEY-2))*CELLSIZE;
+    monster.x= (rand()%((MAP_WIDTH-3)/2)+16)*BLOCK_SIZE;
+    monster.y= (rand()%(MAP_HEIGHT-2))*BLOCK_SIZE;
     monster.state= PEACEFULL;
     monster.healthPoint= 3; 
     monster.speed=1; 
@@ -33,32 +33,3 @@ void readMarkovFromFile(char * filename){
     }
     fclose(fp);
 }
-
-
-void moveMonsterPeacefull(int markov){
-    srand(time(NULL)); 
-
-    probability= rand()%100;
-
-    state=monster.stateMarkov;
-
-    pDead=markov[state][ENTITIES_UP];
-    pSparkle=(markov[state][ENTITIES_UP]+markov[state][ENTITIES_DOWN]);
-
-    pMedium=(markov[state][ENTITIES_UP]+markov[state][ENTITIES_DOWN]+markov[state][ENTITIES_LEFT]);
-
-    pStrong=pMedium+markov[state][ENTITIES_RIGHT]);
-
-
-    if ((0<=probability)&&(probability<pDead)){(listTemporary->fire).state=DEAD;}
-    else if ((pDead<=probability)&&(probability<pSparkle)){(listTemporary->fire).state=SPARKLE;}
-    else if ((pSparkle<=probability)&&(probability<pMedium)){(listTemporary->fire).state=MEDIUM;}
-    else if ((pMedium<=probability)&&(probability<pStrong)){(listTemporary->fire).state=STRONG;}
-    else {(listTemporary->fire).state=SPREAD;}
-
-
-    
-    
-}
-
-void moveMonsterAngry()
