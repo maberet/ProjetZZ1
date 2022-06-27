0f51b39809fab0442f40ce0c5064d7d444fce743
belkhiritaha <belkhiri.taha53@gmail.com>
2022-06-27 11:40:56 +0200

    ajout de la fonction pour dessiner les rayons

--- travail_de_groupe/jeu_appren_par_renfo/src/render.h
+++ travail_de_groupe/jeu_appren_par_renfo/src/render.h
@@ -3,13 +3,23 @@
 
 #include "main.h"
 #include "gest_event.h"
+#include "map.h"
 
 #define FPS_TO_GET 60
+#define CELL_SIZE 10
+ 
+#define DOF 8
+#define BLOCK_SIZE 64
+#define DR 0.0174533
+#define FOV_ANGLE 60
+#define pi 3.14159265358979323846
+#define NB_RAYS (screenDimension.x)
+#define ANGLE_INC ((DR * FOV_ANGLE) / NB_RAYS)
 
 extern SDL_Window *window;
 extern SDL_Renderer *renderer;
 
 
-void MainLoop();
+void mainLoop();
 
 #endif 
