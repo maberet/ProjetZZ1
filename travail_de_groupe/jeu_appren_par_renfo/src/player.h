f2a824f85ea0f5b2ea2856f0e0ed9b324358d483
maberet <maberet@turing.local.isima.fr>
2022-06-27 11:25:32 +0200

    Ajout des structures monster et player

---
+++ travail_de_groupe/jeu_appren_par_renfo/src/player.h
@@ -0,0 +1,23 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#define PLAYER_UP 0
+#define PLAYER_DOWN 1
+#define PLAYER_LEFT 2
+#define PLAYER_RIGHT 3
+
+typedef struct player{
+    int x;
+    int y;
+    int speed;
+    int isMoving;
+    int direction;
+    int HPMax;
+    int currentHP;
+    int coins;
+    float angle;
+} player_t;
+
+extern player_t player;
+
+#endif
